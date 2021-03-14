384b28fa62c296cce27567dbc7a43bc0d7076cec
Mateus <mateusv@outlook.com.br>
2021-03-14 18:16:10 -0300

    Iniciando um blog pessoal! :)

---
+++ blogPessoalMateusV/src/main/java/org/generation/blogPessoalMateusV/BlogPessoalMateusVApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoalMateusV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoalMateusVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoalMateusVApplication.class, args);
+	}
+
+}
